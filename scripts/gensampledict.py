070a89800f8f054eb86c7018424c52f53a6f3655
Pete Bachant <petebachant@gmail.com>
2015-11-09 15:07:24 -0500

    Sample mean profiles behind both turbines

--- scripts/gensampledict.py
+++ scripts/gensampledict.py
@@ -9,9 +9,9 @@ import os
 import sys
 
 # Input parameters
-setformat = "raw"
+setformat = "csv"
 interpscheme = "cellPoint"
-fields = ["U", "vorticity"]
+fields = ["UMean", "kMean"]
 R = 0.45
 D = R*2
 x_D = 1.0
@@ -21,6 +21,7 @@ ny = 41
 z_R_max = 1.6
 z_R_min = -1.6
 nz = 21
+x_turbine2 = 2.682
 
 header = r"""/*--------------------------------*- C++ -*----------------------------------*\
 | =========                 |                                                 |
@@ -44,37 +45,40 @@ def main():
     zmin = z_R_min*R
     zmax = z_R_max*R
     z_array = np.linspace(zmin, zmax, nz)
-    
+
     ymax = y_R_max*R
     ymin = y_R_min*R
-    
-    x = x_D*D
-    
-    txt = header + "\n" 
+
+    txt = header + "\n"
     txt += "setFormat " + setformat + "; \n\n"
     txt += "interpolationScheme " + interpscheme + "; \n\n"
     txt += "sets \n ( \n"
-    
-    for z in z_array:
-        txt += "    " + "profile_" + str(z/R) + "\n"
-        txt += "    { \n"
-        txt += "        type        uniform; \n"
-        txt += "        axis        y; \n"
-        txt += "        start       (" + str(x) + " " + str(ymin) + " " + str(z) + ");\n"
-        txt += "        end         (" + str(x) + " " + str(ymax) + " " + str(z) + ");\n"
-        txt += "        nPoints     " + str(ny) + ";\n    }\n\n"
-        
+
+    for turbine in ["turbine1", "turbine2"]:
+        x = x_D*D
+        if turbine == "turbine2":
+            x += x_turbine2
+        for z in z_array:
+            txt += "    " + turbine + "_profile_" + str(z/R) + "\n"
+            txt += "    { \n"
+            txt += "        type        uniform; \n"
+            txt += "        axis        y; \n"
+            txt += "        start       (" + str(x) + " " + str(ymin) + " " + str(z) + ");\n"
+            txt += "        end         (" + str(x) + " " + str(ymax) + " " + str(z) + ");\n"
+            txt += "        nPoints     " + str(ny) + ";\n    }\n\n"
+
     txt += ");\n\n"
     txt += "fields \n(\n"
-    
+
     for field in fields:
         txt += "    " + field + "\n"
-        
+
     txt += "); \n\n"
     txt += "// *********************************************************************** // \n"
-    
+
     with open("system/sampleDict", "w") as f:
         f.write(txt)
+
 
 if __name__ == "__main__":
     main()
